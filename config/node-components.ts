e233e01ff9c0dfcc0694ab262add80588ade44dd
SY <1129385201@qq.com>
2026-01-04 21:31:33 +0800

    feat: node selector

--- config/node-components.ts
+++ config/node-components.ts
@@ -1,3 +1,5 @@
+import { HttpRequestNode } from "@/components/features/executions/http-request/node";
+import { ManualTriggerNode } from "@/components/features/triggers/manual-trigger/node";
 import { InitialNode } from "@/components/initial-node";
 import { NodeType } from "@/lib/generated/prisma/enums";
 
@@ -5,6 +7,8 @@ import type { NodeTypes } from "@xyflow/react";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
+  [NodeType.HTTP_REQUEST]: HttpRequestNode,
+  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
 } as const satisfies NodeTypes;
 
 export type RegistedNodeType = keyof typeof nodeComponents;
