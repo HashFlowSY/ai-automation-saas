236c349eb85069dbdb30a0d22fc5671cb46ba7ca
SY <1129385201@qq.com>
2026-01-14 20:15:56 +0800

    feat

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -14,6 +14,7 @@ import { discordChannel } from "./channels/discord";
 export const executeWorkflow = inngest.createFunction(
   {
     id: "execute-workflow",
+    retries: process.env.NODE_ENV === "production" ? 3 : 0,
     onFailure: async ({ event, step }) => {
       return prisma.execution.update({
         where: {
